fa32809be7d04d3c9d2bd1ea1408ae49aa0c5a3a
bitturana-ab <b2ra830@gmail.com>
2025-07-30 22:00:22 +0530

    frontend params all complete and start backend api for sign/login

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -5,7 +5,9 @@ export default function userProfilePage({ params }: any) {
         Profile page
       </h1>
       <hr />
-      <h1 className="text-3xl p-4">profile of {params.id}</h1>
+      <h1 className="bg-gray-600 rounded m-3 p-4 text-3xl flex items-center justify-center">
+        profile of {params.id}
+      </h1>
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@ export default function profilePage() {
   return (
     <div>
       <h1 className="bg-gray-800 rounded m-3 p-4 text-3xl flex items-center justify-center">
-        Profile page
+        Default Profile page
       </h1>
     </div>
   );
